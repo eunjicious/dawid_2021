47841316facec27e7ccacc9066477681ff0846ff
Eunji Lee <eunjicious@gmail.com>
2021-07-05 14:21:01 +0900

    add result

--- expr/micro/0_extract.sh
+++ expr/micro/0_extract.sh
@@ -1,22 +1,31 @@
+version="dawid orig"
 workload="rw sw"
-qsize="1 128 256 512 1024 2048"
+qsize="1 128 256 512 1024 2048 4096 8192 16384"
 prot_ratio=10
 trc_dir=./perf_result
 
+#for vs in $version; do
 for wk in $workload; do
 	rfname="$wk".perf
 	mv $rfname $rfname.bak
-	echo "buff-size dawid" > $rfname
+	echo "buff-size orig dawid" > $rfname
 
 	for qs in $qsize; do
 		echo "$wk $qs ..."
-		prefix=$trc_dir/dawid_"$wk"_"$qs"_"$prot_ratio"
-		iops=`grep -rn "IOPS" "$prefix"*.perf | awk '{print $2}' | sort -n | head -n 5 | tail -n 1`
-		echo "$qs" "$iops" >> $rfname
-	done	
 
+		#prefix=$trc_dir/"$vs"_"$wk"_"$qs"_"$prot_ratio"
+		dawid_prefix=$trc_dir/dawid_"$wk"_"$qs"_"$prot_ratio"
+		orig_prefix=$trc_dir/orig_"$wk"_"$qs"_"$prot_ratio"
+
+		dawid_iops=`grep -rn "IOPS" "$dawid_prefix"*.perf | awk '{print $2}' | sort -n | head -n 5 | tail -n 1`
+		orig_iops=`grep -rn "IOPS" "$orig_prefix"*.perf | awk '{print $2}' | sort -n | head -n 5 | tail -n 1`
+		
+		echo "$qs" "$orig_iops" "$dawid_iops" >> $rfname
+	done	
+	
 	cat $rfname
 done
+#done
 
 
 
--- expr/move.sh
+++ expr/move.sh
@@ -1,10 +1,11 @@
 workload="rand_w seq_w"
-bsize="1 128 256 512 1024 2048"
+bsize="1 128 256 512 1024 2048 4096 8192 16384"
 
 
 for wk in $workload; do
 	for bs in $bsize; do
-		sdir=./$wk/$bs
+#		sdir=./$wk/$bs
+		sdir=~/dawid_2021/FlashFTLDriver/eval/"$wk"/"$bs"
 		ddir=./micro/perf_result
 
 		echo "mv $sdir/*.perf $ddir"
