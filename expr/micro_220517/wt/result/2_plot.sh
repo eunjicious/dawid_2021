93852a647aa2b86a7d301c612f7410af4cab42e5
kimleeju <kimleeju@gmail.com>
2022-05-17 19:18:45 +0900

    added stacked graph

---
+++ expr/micro_220517/wt/result/2_plot.sh
@@ -0,0 +1,32 @@
+
+#benchs="rw sw synths synthr synthz realw realf realw_1 realf_1 reallr_1 realtpc_1"
+#benchs="realw_opt_1 realf_opt_1 reallr_opt_1 realtpc_opt_1"
+#benchs="realw_opt realf_opt reallr_opt realtpc_opt" 
+benchs="total.rslt"
+
+#for bench in $@;
+for bench in $benchs;
+do
+	datafile="$bench"
+	outfile="$bench".eps
+	graphtitle="$bench"
+	
+	ylabel="Number_Of_WriteTraffic"
+	xlabel="Target"
+
+#ymax=$(cat "$bench".txt | awk '{print $2}'| sort -nr | head -n 1 | awk '{print $1 * 1.5}')
+	ymax=$(cat "$bench" | awk '{print $2}'|sort -nr | head -n 1 | awk '{print $1 * 1.15}')
+	echo $ymax
+	opt_list="`echo '-'``echo 'e'` `echo datafile=\'$datafile\'`"
+	opt_list="$opt_list `echo '-'``echo 'e'` `echo outfile=\'$outfile\'`"
+	opt_list="$opt_list `echo '-'``echo 'e'` `echo gtitle=\'$graphtitle\'`"
+	opt_list="$opt_list `echo '-'``echo 'e'` `echo gylabel=\'$ylabel\'`"
+	opt_list="$opt_list `echo '-'``echo 'e'` `echo gxlabel=\'$xlabel\'`"
+	opt_list="$opt_list `echo '-'``echo 'e'` `echo gymax=\'$ymax\'`"
+	
+	gnuplot $opt_list perf_wt.cfg && open $outfile
+
+done
+
+
+
