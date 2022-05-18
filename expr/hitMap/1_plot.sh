cb3b6a17bd9796d0fe8fda9dd44a62a0b4d95344
Eunji Lee <eunjicious@gmail.com>
2022-05-18 20:39:49 +0900

    stop here today

--- expr/hitMap/1_plot.sh
+++ expr/hitMap/1_plot.sh
@@ -5,6 +5,7 @@
 #benchs="ssdtrace-01.blk_w.trc.rslt"
 #benchs="2016021616-LUN3.csv_w.trc.rslt 2016021616-LUN4.csv_w.trc.rslt 2016021618-LUN3.csv_w.trc.rslt fileserver_map.trc.rslt linkbench_r_map.trc.rslt ssdtrace-00.blk_w.trc.rslt ssdtrace-01.blk_w.trc.rslt webserver_map.trc.rslt"
 workload="SEQ RAND JESD OLTP" 
+workload="JESD OLTP"
 policy="FIFO DAWID"
 
 #for bench in $@;
@@ -12,16 +13,23 @@ for pl in $policy;do
 	for wk in $workload;do
 	        datafile=./dat/"$wk"_"$pl".dat
 	        #datafile=./dat/"$bench"
-	        outfile=./eps/"$wk"_"$pl"_fix.eps
+	        outfile=./eps/"$wk"_"$pl".eps
 	#       graphtitle="$bench"
-	        graphtitle="Hit-Ratio-in-Write-Buffer"
+#	        graphtitle="Hit-Ratio-in-Write-Buffer"
    	 
 	       	 ylabel="hit-ratio(Normalization)"
 	        xlabel="Ratio(footprint)"
 	
 	        #ymax=$(cat "$bench".dat | awk '{print $2}'| sort -nr | head -n 1 | awk '{print $1 * 1.5}')
-	        ymax=$(cat "$datafile" | awk '{print $4}'|sort -nr | head -n 1 | awk '{print $1 * 1.001}')
-	        ymin=$(cat "$datafile" | awk '{print $2}'|sort -nr | tail -n 2 | head -n 1 | awk '{print $1}')
+	        #ymax=$(cat "$datafile" | awk '{print $4}'|sort -nr | head -n 1 | awk '{print $1 * 1.001}')
+			ymax=1.0
+	        #ymin=$(cat "$datafile" | awk '{print $2}'|sort -nr | tail -n 2 | head -n 1 | awk '{print $1}')
+			
+			if [[ $wk == "JESD" ]]; then
+				ymin=0.7
+			else
+				ymin=0.9
+			fi
 	#       echo $ymin
 	#       echo $ymax
 	
